63958997c4cad8be035259d35853e8c86671b5f8
Alejandro Hernandez Ferriz <ahferriz@gmail.com>
2018-03-14 12:15:30 -0600

    Adici+on de botón de logout a mensaje en admin.

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -9,6 +9,7 @@
                         <div class="box box-danger">
                             <div class="box-body">
                                 <h4><i class="fa fa-ban"></i> &nbsp; El sistema de clientes no cuenta con interfaz de administración.</h4>
+                                <a href="{{ route('logout') }}" role="button" class="btn btn-default"><i class="fa fa-sign-out"></i> Salir</a>
                             </div>
                         </div>
                     </div>
