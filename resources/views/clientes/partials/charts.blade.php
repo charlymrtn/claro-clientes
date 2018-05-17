1b39e9cde96b24b2b5cb15285c66157081db0729
Alejandro Hernandez Ferriz <ahferriz@gmail.com>
2018-05-17 12:35:30 -0500

    Corrección de referencias a assets.

--- resources/views/clientes/partials/charts.blade.php
+++ resources/views/clientes/partials/charts.blade.php
@@ -1,4 +1,4 @@
     <!-- Partial: Charts includes -- [ -->
-        <link rel="stylesheet" type="text/css" href="/css/mix/charts.css">
-        <script type="text/javascript" src="/js/mix/charts.js"></script>
+        <link rel="stylesheet" type="text/css" href="{{ mix('/css/mix/charts.css') }}">
+        <script type="text/javascript" src="{{ mix('/js/mix/charts.js') }}"></script>
     <!-- Partial: Charts -- ] -->
